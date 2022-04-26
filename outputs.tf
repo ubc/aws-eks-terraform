73c90fbe109d87a39b846c0d320e30e195977dd1
Rahim Khoja <rahim@khoja.ca>
2022-04-26 14:11:22 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -13,6 +13,10 @@ output "efs_id" {
   value       = aws_efs_file_system.home.id
 }
 
+output "eks_cluster_ssh_private_key_pem" {
+  value = tls_private_key.ssh.private_key_pem
+}
+
 ################################################################################
 # VPC
 ################################################################################
