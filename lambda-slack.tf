9dd98f2d80b972e41e62fe44a82a26f71c71f9e7
Balaji Srinivasa Rao <balaji.srinivasarao@ubc.ca>
2023-02-07 17:44:21 -0500

    fixing commented changes minus alerts_enabled flag

--- lambda-slack.tf
+++ lambda-slack.tf
@@ -1,6 +1,7 @@
 resource "aws_iam_role" "lambda_role" {
-name   = "slack-lambda-role"
-assume_role_policy = <<EOF
+  name               = "slack-lambda-role"
+  count              = var.alerts_enabled ? 1 : 0
+  assume_role_policy = <<EOF
 {
  "Version": "2012-10-17",
  "Statement": [
@@ -18,11 +19,11 @@ EOF
 }
 
 resource "aws_iam_policy" "iam_policy_for_lambda" {
- 
- name         = "aws_iam_policy_for_terraform_aws_lambda_role"
- path         = "/"
- description  = "AWS IAM Policy for managing aws lambda role"
- policy = <<EOF
+
+  name        = "aws_iam_policy_for_terraform_aws_lambda_role"
+  path        = "/"
+  description = "AWS IAM Policy for managing aws lambda role"
+  policy      = <<EOF
 {
  "Version": "2012-10-17",
  "Statement": [
@@ -41,23 +42,23 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
- role        = aws_iam_role.lambda_role.name
- policy_arn  = aws_iam_policy.iam_policy_for_lambda.arn
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.iam_policy_for_lambda.arn
 }
 
 data "archive_file" "zip_lambda" {
-type        = "zip"
-output_path = "lambda-slack/slack.zip"
-source_dir = "${path.module}/lambda-slack/package"
+  type        = "zip"
+  output_path = "lambda-slack/slack.zip"
+  source_dir  = "${path.module}/lambda-slack/package"
 }
 
 resource "aws_lambda_function" "slack_lambda" {
-filename                       = "${path.module}/lambda-slack/slack.zip"
-function_name                  = "slack-alert"
-role                           = aws_iam_role.lambda_role.arn
-handler                        = "slack.lambda_handler"
-runtime                        = "python3.8"
-depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+  filename      = "${path.module}/lambda-slack/slack.zip"
+  function_name = "slack-alert"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "slack.lambda_handler"
+  runtime       = "python3.8"
+  depends_on    = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
 
   environment {
     variables = {
