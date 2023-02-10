41a6cbf24525163f6f78431ffd85fccbe1506988
Balaji Srinivasa Rao <balaji.srinivasarao@ubc.ca>
2023-02-10 13:10:03 -0500

    adding count for sns

--- sns.tf
+++ sns.tf
@@ -3,8 +3,9 @@ resource "aws_sns_topic" "cluster_alerts" {
 }
 
 resource "aws_sns_topic_subscription" "cluster_alerts_subscription" {
-    topic_arn = aws_sns_topic.cluster_alerts.arn
-    protocol = "lambda"
-    endpoint = aws_lambda_function.slack_lambda.arn
-  
+  topic_arn = aws_sns_topic.cluster_alerts.arn
+  count     = var.alerts_enabled ? 1 : 0
+  protocol  = "lambda"
+  endpoint  = aws_lambda_function.slack_lambda.arn
+
 }
