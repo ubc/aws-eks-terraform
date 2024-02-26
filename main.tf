ccb7448e687320f4efee6ba4a30f09dda748ec17
Pan Luo <pan.luo@ubc.ca>
2024-02-26 15:19:21 -0800

    Update to MariaDB 10.6

--- main.tf
+++ main.tf
@@ -60,13 +60,13 @@ resource "aws_db_instance" "rds" {
   identifier             = "rds-db-${local.cluster_name}"
   allocated_storage      = 20
   storage_type           = "gp2"
-  engine                 = "mysql"
-  engine_version         = "5.7"
-  instance_class         = "db.t2.micro"
+  engine                 = "mariadb"
+  engine_version         = "10.6"
+  instance_class         = "db.t3.micro"
   db_name                = "jupyterhub"
   username               = "admin"
   password               = "UBC-Shib-Backend"
-  parameter_group_name   = "default.mysql5.7"
+  parameter_group_name   = "default.mariadb10.6"
   publicly_accessible    = false
   skip_final_snapshot    = true
   vpc_security_group_ids = [aws_security_group.rds_mysql.id]
